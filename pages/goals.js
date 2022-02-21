37a85e998c0833850c7b70c566cab2b30f88b4cf
DominikBackhouse <b9019847@my.shu.ac.uk>
2022-02-21 11:01:19 +0000

    added goals.js

---
+++ pages/goals.js
@@ -0,0 +1,10 @@
+import React from "react"
+import {StyleSheet, View, Text} from "react-native"
+
+export default function Goals(){
+  return (
+    <View style={StyleSheet.container}>
+      <Text>Goals</Text>
+    </View>
+  )
+}
---
+++ pages/homepage.js
@@ -0,0 +1,8 @@
+import { createStackNavigator} from 'react-navigation-stack';
+import { createAppContainer} from 'react-navigation';
+
+const screens = {
+
+}
+
+const HomeStack = createStackNavigator(screens);
