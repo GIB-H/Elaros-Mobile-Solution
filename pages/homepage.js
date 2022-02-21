6c28966a2ebf1d3c787a4a0cf2f6eac84733297c
DominikBackhouse <b9019847@my.shu.ac.uk>
2022-02-21 11:22:46 +0000

    update to homepage.js

--- pages/homepage.js
+++ pages/homepage.js
@@ -2,7 +2,9 @@ import { createStackNavigator} from 'react-navigation-stack';
 import { createAppContainer} from 'react-navigation';
 
 const screens = {
-
+    Home: {
+        screen: Home
+    }
 }
 
 const HomeStack = createStackNavigator(screens);
