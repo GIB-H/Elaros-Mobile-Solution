14d0994f9f7c25b244bffc76b8d5008abe21d053
Dominik Backhouse <b9019847@my.shu.ac.uk>
2022-02-22 10:53:40 +0000

    update homeStack.js and App.js

--- App.js
+++ App.js
@@ -1,15 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Navigator from '../routes/homeStack.js';
 
 export default function App() {
 
-  console.log("App executed.") // remove in full version
-
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Navigator />
   );
 }
 
--- routes/homeStack.js
+++ routes/homeStack.js
@@ -4,12 +4,34 @@ import { createAppContainer} from 'react-navigation';
 import createGoal from '../screens/createGoal';
 import goals from '../screens/goals';
 import graphs from '../screens/graphs';
-import Home from '../screens/home';
+import home from '../screens/home';
 import login from '../screens/login';
 import stats from '../screens/stats';
-
+import signup from '../screens/signUp';
 
 const screens = {
+    login: {
+        screen: login
+    },
+    home: {
+        screen: home
+    },
+    stats: {
+        screen: stats
+    },
+    graphs: {
+        screen: graphs
+    },
+    goals: {
+        screen: goals
+    },
+    createGoal: {
+        screen: createGoal
+    },
+    signup: {
+        screen: signup
+    }
 }
 
 const HomeStack = createStackNavigator(screens);
+export default createAppContainer(homeStack);
